d38820a2534dfbdace355ffff86037a605dcf861
zuruzirou <zuruzirou@gmail.com>
2022-06-16 09:46:59 +0900

    Delete abc directory

--- abc/Hoge2.java
+++
@@ -1,8 +0,0 @@
-package abc;
-
-public class Hoge2 {
-    
-    public static void main(String[] args){
-        System.out.println("Hoge2!!");
-    }
-}
--- abc/Hoge2Test.java
+++
@@ -1,9 +0,0 @@
-package abc;
-
-
-public class Hoge2Test {
-    @Test
-    public void testAbc(){
-
-    }
-}
